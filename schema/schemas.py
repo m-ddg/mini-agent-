7c49d4066c67c2dba8d766143a22d99c0767a960
m-ddg <101467820+m-ddg@users.noreply.github.com>
2026-04-10 01:31:52 +0800

    大改了schema以适配responses api格式，主要增加了Event类，并将ToolCall改为了扁平结构

--- schema/schemas.py
+++ schema/schemas.py
@@ -1,32 +1,40 @@
 from pydantic import BaseModel
-from typing import Any
+from typing import Any, Literal
+from ..tools import ToolResult
 from enum import StrEnum
 
 
 class Provider(StrEnum):
     """ LLM提供商 """
+
     OPENAI = 'openai'
     ANTHROPIC = 'anthropic'
 
 
-class FunctionCall(BaseModel):
-    """ 函数调用封装格式 """
-    arguments: dict[str, Any]
-
-
 class ToolCall(BaseModel):
     """ 工具调用封装格式 """
+
     id: str
     name: str
     type: str = 'function'   # 目前仅有调用函数 type = function
-    function: FunctionCall
+    arguments: dict[str, Any]
 
+
+class Event(BaseModel):
+    """ 用于适配事件驱动的responses api LLM调用 """
+
+    type: Literal["message", "reasoning", "function_call", "function_call_output"]
+    text: str | None = None  # 当事件类型为message时，该属性记录LLM返回的文字
+    thinking: str | None = None  # 当事件类型为reasoning时，该属性记录LLM的推理内容
+    tool_call: ToolCall | None = None  # 当事件类型为function_call时，该属性记录LLM的工具调用
+    tool_result: ToolResult | None = None
 
 class Message(BaseModel):
     """ 框架内消息结构 """
-    role: str # system, user, assistant, tool
+
+    role: Literal["system", "user", "assistant", "tool"]
     thinking: str | None = None
-    content: str | list[dict[str, Any]]
+    content: str | list[Event]   # 当用于responses api client时，content是存储有事件的列表
     tool_calls: list[ToolCall] | None = None
     # 当role为tool时，下面两个属性的值不应为空，tool_calls需要为空
     tool_call_id: str | None  = None
@@ -35,6 +43,7 @@ class Message(BaseModel):
 
 class TokenUsage(BaseModel):
     """ 记录token消耗，包括输入、输出和总计的消耗 """
+
     input_tokens: int = 0
     output_tokens: int = 0
     total_tokens: int = 0
@@ -42,8 +51,7 @@ class TokenUsage(BaseModel):
 
 class LLMResponse(BaseModel):
     """ 封装来自LLM的原始返回 """
+
+    message: Message  # 为避免字段重复，且方便从LLMResponse中拿到本轮的事件，故设置此属性
     finish_reason: str
-    thinking: str | None = None
-    content: str
-    tool_calls: list[ToolCall] | None = None
     token_usage: TokenUsage
